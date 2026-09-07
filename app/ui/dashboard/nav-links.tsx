1094d160b2bcf897fee584171694819bc626fc26
KingSlayer3x <alito321a@gmail.com>
2026-09-08 00:37:38 +0300

    added routes

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -9,9 +9,7 @@ import {
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
+    name: 'Invoices', href: '/dashboard/invoices', icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
@@ -27,7 +25,7 @@ export default function NavLinks() {
             href={link.href}
             className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
           >
-            <LinkIcon className="w-6" />
+            <LinkIcon className="w-7" />
             <p className="hidden md:block">{link.name}</p>
           </a>
         );
